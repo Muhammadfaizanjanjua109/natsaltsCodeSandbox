b59b93e6639737e0179bb4af5d0f66bf6eb833ac
Muhammad Faizan <66197447+Muhammadfaizanjanjua109@users.noreply.github.com>
2022-10-18 21:31:08 +0500

    cards changes

--- src/sharedComponents/Cards.js
+++ src/sharedComponents/Cards.js
@@ -1,28 +1,57 @@
-import React from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 function Cards(props) {
+  const Type = props.type;
+  const [hoverState, sethoverState] = useState(false);
+  const MouseLeaving = () => {
+    setTimeout(() => {
+      sethoverState(false);
+    }, 2000);
+  };
   return (
     <>
-      <div class="card-deck container-fluid">
-        {props.data.map((index, item) => {
-          return (
-            <>
-              <div key={item} class="card" style={{ width: "18rem" }}>
-                <img
-                  class="card-img-top"
-                  style={{ height: "220px", objectFit: "contain" }}
-                  src={index.src}
-                  alt={index.alt}
-                />
-                <div class="card-body">
-                  <h5 class="card-title">{index.tittle}</h5>
-                  <p class="card-text">{index.text}</p>
-                  <Link to="/">Buy Now</Link>
+      <div className="hoverDiv  hover">
+        <div
+          class="card-deck container-fluid"
+          onClick={() => sethoverState(true)}
+          onMouseOut={() => MouseLeaving()}
+        >
+          {props.data.map((index, item) => {
+            return (
+              <>
+                <div key={item} class="card" style={{ width: "18rem" }}>
+                  <img
+                    class="card-img-top"
+                    style={{ height: "220px", objectFit: "contain" }}
+                    src={index.src}
+                    alt={index.alt}
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">{index.tittle}</h5>
+                    <p class="card-text">{index.text}</p>
+                    <Link to="/">Buy Now</Link>
+                  </div>
                 </div>
-              </div>
-            </>
-          );
-        })}
+              </>
+            );
+          })}
+        </div>
+
+        <div
+          className="HoverwaliDiv"
+          style={hoverState ? { display: "block" } : { display: "none" }}
+        >
+          <div class="carddeckbuttonDiv">
+            <button
+              onMouseOver={() => sethoverState(true)}
+              class="carddeckbutton btn btn-light"
+            >
+              <Link to="/products" state={{ name: Type }}>
+                Shop Now
+              </Link>
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
