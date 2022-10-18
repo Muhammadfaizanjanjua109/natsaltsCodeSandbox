894e118d6ebf8a0cb0e8e91a74bc871e58ac578d
Muhammad Faizan <66197447+Muhammadfaizanjanjua109@users.noreply.github.com>
2022-10-18 21:30:33 +0500

    added react router dom

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import "./index.css";
 import App from "./App";
 import "./Assets/css/style.css";
 import reportWebVitals from "./reportWebVitals";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Details from "./pages/Details";
+import NoPageFound from "./pages/NoPageFound";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+      </Route>
+      <Route path="/products" element={<Products />}>
+        <Route path="products/:id" element={<Details />} />
+      </Route>
+      <Route path="*" element={<NoPageFound />}></Route>
+    </Routes>
+  </BrowserRouter>,
   <React.StrictMode>
     <App />
   </React.StrictMode>
